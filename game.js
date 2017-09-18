d836047f16b7e95070ccb7f5edb1691523fa61c6
Trenton Potter <trpotter72@gmail.com>
2017-09-18 09:56:05 -0500

    Restructured folder, added BTC mining to game

--- game.js
+++ game.js
@@ -1,65 +1,131 @@
+//******************************************************************************
+//  Declaring Global Variables to keep track of game state
+//******************************************************************************
+
 let money = 0;
-let cost = 10;
+let degree_cost = 10;
 let earn = 1;
+let BTCminers = 0;
+let BTCminer_cost = 10;
+
+//Constants
+const update_rate = 100;
+const BTCminer_earnings = 1;
+
+//******************************************************************************
+//  Declaring JQuery objects
+//******************************************************************************
+
+//JQuery diagnostics variables
+let d_degree_cost = $("<p></p>");
+let d_money = $("<p></p>");
+let d_earn = $("<p></p>");
+let d_BTCminers = $("<p></p>");
+let d_BTCminer_cost = $("<p></p>");
+
+//JQuery Buttons
+let upgrade_btn = $("<button></button>").text("Earn a Degree $" + degree_cost);
+let buy_BTCminer_btn = $("<button></button>").text("Buy BTC miner $" + degree_cost);
+let earn_btn = $("<button></button>").text("Work Day Job $" + earn);
+let money_count = $("<h1></h1>").text("$" + 0);
+
 
 function main() {
   let doc = $("document");
-  let diagnostics = $("<div></div>").css({"position":"fixed", "bottom":"10px", "right":"10px", "outline":"#00ee00 solid 5px", "width": "auto", "height" : "auto"});
+  let diagnostics = $("<div></div>").css({
+    "position":"fixed",
+    "bottom":"10px",
+    "right":"10px",
+    "outline":"#00ee00 solid 5px",
+    "width": "auto",
+    "height" : "auto"});
+  let button_holder = $("<div></div>").css({
+    "position":"float",
+    "width": "auto",
+    "height" : "auto",
+    "align":"center",
+    "text-align":
+    "center" });
   diagnostics.html("<ul id='stats'></ul>");
-    let d_cost = $("<li>Cost: " + cost + "</li>");
-    let d_money = $("<li>Money: " + money + "</li>");
-    let d_earn = $("<li>Earn: " + earn + "</li>");
-    diagnostics.append(d_cost, d_money, d_earn);
+    diagnostics.append(d_degree_cost, d_money, d_earn);
 
-  let upgrade_clicker = $("<button>").text("Earn a Degree $" + cost);
-  upgrade_clicker.on("click", ()=>  {
-    if (money >= cost) {
-      money -= cost;
-      cost *= 2;
+  upgrade_btn.on("click", ()=>  {
+    if (money >= degree_cost) {
+      money -= degree_cost;
+      degree_cost *= 2;
       earn *= 2;
-      earn_btn.text("Work Day Job $" + earn);
-      upgrade_clicker.text("Earn a Degree $" + cost);
-      money_count.text("$" + money);
     }});
 
-  let earn_btn = $("<button></button>").text("Work Day Job $" + earn);
   earn_btn.on("click", () => {
       money += earn;
-      money_count.text("$" + money);
   });
-
-  let money_count = $("<h1></h1>").text("$" + 0);
+  buy_BTCminer_btn.on("click", () => {
+    if (money >= BTCminer_cost) {
+      money -= BTCminer_cost;
+      BTCminer_cost *= 2;
+      BTCminers += 1;
+    }
+  })
   money_count.on('click', () => {
     let red = Math.random()*256;
-    let red_shadow = red - 30;
+    let red_shadow = Math.random()*256;
     console.log("r: " + red);
     let green = Math.random()*256;
-    let green_shadow = green - 30;
+    let green_shadow = Math.random()*256;
     console.log("g: " + green);
     let blue = Math.random()*256;
-    let blue_shadow = blue - 30;
+    let blue_shadow = Math.random()*256;
     console.log("b: " + blue);
 
     let color = "#" + red.toString(16).slice(0,2) + green.toString(16).slice(0,2) + blue.toString(16).slice(0,2);
     console.log(color);
 
-    let color_shadow = "2px 2px 30px #" + red_shadow.toString(16).slice(0,2) + green_shadow.toString(16).slice(0,2) + blue_shadow.toString(16).slice(0,2);
+    let color_shadow = "0px 0px 40px #"
+    + red_shadow.toString(16).slice(0,2)
+    + green_shadow.toString(16).slice(0,2)
+    + blue_shadow.toString(16).slice(0,2)
+    + ",-2px -2px #000000,"
+  		+ "2px 2px #000000,"
+  		+ "2px -2px #000000,"
+  		+ "-2px 2px #000000,"
+  		+ "0px -2px #000000,"
+  		+ "0px 2px #000000,"
+  		+ "2px 0px #000000,"
+  		+ "-2px 0px #000000";
     money_count.css('color',  color );
     money_count.css('text-shadow',  color_shadow);
   });
 
-
-  $("body").append(earn_btn);
-  $("body").append(upgrade_clicker);
   $("body").append(money_count);
+  $("body").append("<br/>");
+  $("body").append(button_holder);
+  button_holder.append(earn_btn);
+  button_holder.append(upgrade_btn);
+  button_holder.append(buy_BTCminer_btn);
   $("body").append(diagnostics);
+  setInterval(()=> {
+    update_screen();
+    update_money();
+  }, update_rate);
 
 }
 
 function update_screen() {
-  d_cost.text(cost);
-  d_money.text(moeny);
-  d_earn.text(earn);
+  d_degree_cost.text("degree_cost: " + degree_cost);
+  d_money.text("money: " + money.toFixed());
+  d_earn.text("earn: " + earn);
+
+  money_count.text("$" + money.toFixed());
+  upgrade_btn.text("Earn a Degree: $" + degree_cost);
+  earn_btn.text("Work Day Job: $" + earn);
+  buy_BTCminer_btn.text("Buy BTC Miner: $" + BTCminer_cost);
+}
+
+function update_money() {
+  let sum = 0;
+  sum += BTCminers*BTCminer_earnings*update_rate/1000
+
+  money+= sum;
 }
 
 $("document").ready( () => {
